155d4ed85f298ff2bc4ffb3bce18a103f75a6760
PrachiRameshGodase <prachigodase2@gmail.com>
2023-04-01 22:50:26 +0530

    storing-all-data

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const item=document.getElementById("item");
+const usersList=document.getElementById("users");
+const username=document.getElementById("item1");
+const emailId=document.getElementById("item2");
 
 item.addEventListener('submit',onsubmit);
 // making function
@@ -6,17 +9,27 @@ function onsubmit(e){
         e.preventDefault();
         const name=e.target.username.value;
         const email=e.target.emailId.value;
-
+//add in local storage
         localStorage.setItem("name",name);
         localStorage.setItem("email",email);
     
 
+//create new variable for creating string like stru
+        const myObj={
+            name,
+            email
+        };
+        let x=JSON.stringify(myObj);
+        localStorage.setItem("userDet",x);
 
-const myObj={
-    name,
-    email
-};
+//create new element store data
+        const li=document.createElement("li");
+        li.appendChild(document.createTextNode(`${username.value} : ${emailId.value}`));
+        usersList.appendChild(li);
+        
+//create fields
+        username.value="";
+        emailId.value="";
 
-let x=JSON.stringify(myObj);
-localStorage.setItem("userDet",x);
-}
+
+    }
